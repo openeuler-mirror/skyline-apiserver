49d9553804c038f317b089c73c1a7d6a5a1ae845
Access_ac <wangjing@uniontech.com>
2026-06-09 15:07:53 +0800

    init 2.0.0 from upstream

---
+++ tools/post_install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -ex
+
+INSTALL_PROJECTS="keystone \
+    placement \
+    nova \
+    cinder \
+    glance \
+    trove \
+    neutron neutron-vpnaas \
+    heat \
+    ironic \
+    octavia \
+    manila \
+    magnum \
+    zun\
+    barbican \
+    designate \
+    masakari"
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+for project in ${INSTALL_PROJECTS}
+do
+    pip install -U git+https://opendev.org/openstack/${project}@${BRANCH}
+done
